eb41c1806b8e636d0e0543200941d55ace83fa7e
Sam Allberg <144385789+samallberg@users.noreply.github.com>
2023-09-07 14:07:28 -0600

    Statement

---
+++ README.m
@@ -0,0 +1,6 @@
+One thing I’ve been able to boast since I was young is my ability to communicate in both verbal and written methods. 
+I have a passion for things that are organized and precise - from my time to the tasks that I perform, and a hunger to learn more and succeed in any position I sit in. 
+I bring with me these passions, as well as a strong set of core values focused around the protection of people and their privacy. 
+These core values have brought me to the world of cybersecurity, where I have sown the garden of my dedication and devotion. 
+This combination of skills, values, and interests have built the foundation you need to build the fortress of your security department.
+
